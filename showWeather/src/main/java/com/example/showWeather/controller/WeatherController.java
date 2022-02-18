ef6c42343053553d25d8c44d7440211978d72492
Marco <marc.trentin06@gmail.com>
2022-02-18 14:32:16 +0100

    US2 : Get weather by City, Country and ZipCode through showWeather gateway (port:8081)

---
+++ showWeather/src/main/java/com/example/showWeather/controller/WeatherController.java
@@ -0,0 +1,71 @@
+package com.example.showWeather.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class WeatherController {
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String  fallbackMethod(String parameter){
+        return "Fallback response:: No weather details available temporarily. Service is probably down...";
+    }
+
+
+    @RequestMapping(value = "weatherDetails/city/{city}", method = RequestMethod.GET)
+    @HystrixCommand(fallbackMethod = "fallbackMethod")
+    public String getWeatherByCity(@PathVariable String city)
+    {
+        System.out.println("Getting Weather details for city : " + city);
+
+        String response = restTemplate.exchange("http://localhost:8080/city/{city}",
+                HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}, city).getBody();
+
+        System.out.println("Response Body " + response);
+
+        return "Weather city -  " + city + " [ Weather Details " + response+" ]";
+    }
+
+    @RequestMapping(value = "weatherDetails/zipCode/{zipCode}", method = RequestMethod.GET)
+    @HystrixCommand(fallbackMethod = "fallbackMethod")
+    public String getWeatherByZipCode(@PathVariable String zipCode)
+    {
+        System.out.println("Getting Weather details for zipCode : " + zipCode);
+
+        String response = restTemplate.exchange("http://localhost:8080/zipCode/{zipCode}",
+                HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}, zipCode).getBody();
+
+        System.out.println("Response Body " + response);
+
+        return "Weather zipCode -  " + zipCode + " [ Weather Details " + response+" ]";
+    }
+
+    @RequestMapping(value = "weatherDetails/country/{country}", method = RequestMethod.GET)
+    @HystrixCommand(fallbackMethod = "fallbackMethod")
+    public String getWeatherByCountry(@PathVariable String country)
+    {
+        System.out.println("Getting Weather details for country : " + country);
+
+        String response = restTemplate.exchange("http://localhost:8080/country/{country}",
+                HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}, country).getBody();
+
+        System.out.println("Response Body " + response);
+
+        return "Weather country -  " + country + " [ Weather Details " + response+" ]";
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }}
---
+++ showWeather/src/test/java/com/example/showWeather/ShowWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.showWeather;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ShowWeatherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
